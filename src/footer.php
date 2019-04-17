f15640e20e344fbc45ad1bd0580d696d473e89d2
Phil Schanely <phil.schanely@dockyard.com>
2019-04-16 23:35:13 -0400

    Deployment setup

--- src/footer.php
+++ src/footer.php
@@ -1,18 +1,10 @@
 
 <footer>
-    <ul class="footer-spot-widgets list-unstyled">
-    <?php dynamic_sidebar('footer-spot'); ?>
-    </ul>
-<!---->
-<!--    <p class="footmatter">Located in Cedarville, Ohio. Serving Dayton and the surrounding areas. <br/>Copyright &copy; 2017 by Karen Schanely. <br/>All rights reserved.</p>-->
-<!--    <ul class="links">-->
-<!--        <li class="icon-instagram"><a href="https://www.instagram.com/karen.schanely/" target="_blank"><img src="/wp-content/uploads/Instagram-black.svg" alt="View my Instagram Profile" /></a></li>-->
-<!--        <li class="icon-facebook"><a href="https://www.facebook.com/KarenSchanely/" target="_blank"><img src="/wp-content/uploads/Facebook-black.svg" alt="Find me on Facebook" /></a></li>-->
-<!--    </ul>-->
+  <ul class="footer-spot-widgets list--unstyled">
+  <?php dynamic_sidebar('footer-spot'); ?>
+  </ul>
 </footer>
 <?php wp_footer(); ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/scripts/enquire.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/scripts/scripts.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/app.js"></script>
 </body>
 </html>
--- src/single.php
+++ src/single.php
@@ -1,26 +1,26 @@
 <?php get_header(); ?>
-    <main class="article">
-        <?php if (have_posts()) :?>
-            <?php while(have_posts()) : the_post(); ?>
-                <?php $custom_fields = get_post_custom($post_id); ?>
-                <h2><?php the_title(); ?></h2>
-                <p class="article-date"><?php echo get_the_date('M d, Y'); ?></p>
-                <?php the_content(); ?>
-                <?php if (array_key_exists('blog-c2a-prompt', $custom_fields)) : ?>
-                <p class="lead"
-                   style="text-align:center;">
-                    <?php echo $custom_fields['blog-c2a-prompt'][0]; ?>
-                </p>
-                <?php endif; ?>
-                <?php if (array_key_exists('blog-c2a-button', $custom_fields)) : ?>
-                <a class="btn" style="width: 200px; margin: 0 auto; display: block;"
-                   href="http://schanelyphotography.com/contact/">
-                    <?php echo $custom_fields['blog-c2a-button'][0]; ?> &raquo;
-                </a>
-                <?php endif; ?>
-            <?php endwhile; ?>
-        <?php else : ?>
-            <p>Page not found.</p>
-        <?php endif; ?>
-    </main>
+<main class="page-article__main">
+  <?php if (have_posts()) :?>
+    <?php while(have_posts()) : the_post(); ?>
+      <?php $custom_fields = get_post_custom($post_id); ?>
+      <h2><?php the_title(); ?></h2>
+      <p class="article-date"><?php echo get_the_date('M d, Y'); ?></p>
+      <?php the_content(); ?>
+      <?php if (array_key_exists('blog-c2a-prompt', $custom_fields)) : ?>
+        <p class="lead"
+           style="text-align:center;">
+          <?php echo $custom_fields['blog-c2a-prompt'][0]; ?>
+        </p>
+      <?php endif; ?>
+      <?php if (array_key_exists('blog-c2a-button', $custom_fields)) : ?>
+        <a class="btn" style="width: 200px; margin: 0 auto; display: block;"
+           href="http://schanelyphotography.com/contact/">
+            <?php echo $custom_fields['blog-c2a-button'][0]; ?> &raquo;
+        </a>
+      <?php endif; ?>
+    <?php endwhile; ?>
+  <?php else : ?>
+    <p>Page not found.</p>
+  <?php endif; ?>
+</main>
 <?php get_footer(); ?>
